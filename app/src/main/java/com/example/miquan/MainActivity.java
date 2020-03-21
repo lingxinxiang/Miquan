19e13f2d34a5cf7d7d27bb1b050b6fb62ca0c2cd
Your Name <you@example.com>
2020-03-21 15:48:56 +0800

    初始化

--- app/src/main/java/com/example/miquan/MainActivity.java
+++ app/src/main/java/com/example/miquan/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.miquan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    String url = "https://suzhougs.pang118.com/api/device/queryDeviceSaleProducts?deviceId=1077";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
