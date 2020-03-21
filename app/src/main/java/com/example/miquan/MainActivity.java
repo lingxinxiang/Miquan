74adbf7be3eaad14b4c0d362be9b5ac6512448e8
Your Name <you@example.com>
2020-03-21 17:11:46 +0800

    跳转

--- app/src/main/java/com/example/miquan/MainActivity.java
+++ app/src/main/java/com/example/miquan/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.miquan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    String url = "https://suzhougs.pang118.com/api/device/queryDeviceSaleProducts?deviceId=1077";
+    private static final String TAG = "TEST";
+  //  String url = "https://suzhougs.pang118.com/api/device/queryDeviceSaleProducts?deviceId=1077";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.i(TAG, "onCreate");
+
+        ImageView imageView = findViewById(R.id.iv_head);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
     }
 }
